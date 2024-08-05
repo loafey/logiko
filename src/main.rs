385b9d779b3f694abab8144957fe3bf5d4bdf9d6
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-08-06 00:43:18 +0200

    Fix being able to select the result field.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,19 @@ fn Term<T: 'static + PartialEq + std::fmt::Display + Clone>(
     term: Ptr<Logic<T>>,
     outer: bool,
     index: Vec<usize>,
+    unselectable: bool,
 ) -> Element {
-    let mut index_var = use_context::<Signal<Vec<usize>>>();
+    let mut index_var = use_context::<Signal<Option<Vec<usize>>>>();
+    let class = if let Some(map) = index_var.read().as_ref() {
+        map == &index
+    } else {
+        false
+    };
+    let class = if class && !unselectable {
+        "term term-selected"
+    } else {
+        "term"
+    };
     let index0 = {
         let mut c = index.clone();
         c.push(0);
@@ -29,42 +40,49 @@ fn Term<T: 'static + PartialEq + std::fmt::Display + Clone>(
     let term_gui = match &*term.borrow() {
         Logic::Variable(v) => rsx!("{v}"),
         Logic::And(a, b) => rsx!(
-            Term {term: a.clone(), outer: false, index: index0}
+            Term {term: a.clone(), outer: false, index: index0, unselectable}
             " ∧ "
-            Term {term: b.clone(), outer: false, index: index1}
+            Term {term: b.clone(), outer: false, index: index1, unselectable}
         ),
         Logic::Or(a, b) => rsx!(
-            Term {term: a.clone(), outer: false, index: index0}
+            Term {term: a.clone(), outer: false, index: index0, unselectable}
             " ∨ "
-            Term {term: b.clone(), outer: false, index: index1}
+            Term {term: b.clone(), outer: false, index: index1, unselectable}
         ),
         Logic::Implies(a, b) => rsx!(
-            Term {term: a.clone(), outer: false, index: index0}
+            Term {term: a.clone(), outer: false, index: index0, unselectable}
             " → "
-            Term {term: b.clone(), outer: false, index: index1}
+            Term {term: b.clone(), outer: false, index: index1, unselectable}
         ),
         Logic::Equivalent(_, _) => rsx!("Equivalent"),
         Logic::Not(t) if matches!(&*t.borrow(), Logic::Variable(_)) => {
-            rsx!("¬ " Term { term: t.clone(), outer: true, index: index0 })
+            rsx!("¬ " Term { term: t.clone(), outer: true, index: index0, unselectable })
         }
-        Logic::Not(t) => rsx!("¬ " Term { term: t.clone(), outer: false, index: index0 }),
+        Logic::Not(t) => {
+            rsx!("¬ " Term { term: t.clone(), outer: false, index: index0, unselectable })
+        }
         Logic::Bottom => rsx!("⊥"),
         Logic::Empty => rsx!("×"),
     };
-    let on_click = move |e: Event<MouseData>| {
-        e.stop_propagation();
-        *index_var.write() = index.clone();
+    let on_click = {
+        move |e: Event<MouseData>| {
+            e.stop_propagation();
+            if !unselectable {
+                *index_var.write() = Some(index.clone());
+            }
+        }
     };
+
     if matches!(&*term.borrow(), Logic::Variable(_)) || outer {
         rsx!(div {
             onclick: on_click,
-            class: "term",
+            class: class,
             {term_gui}
         })
     } else {
         rsx!(div {
             onclick: on_click,
-            class: "term",
+            class: class,
             "( " {term_gui} " )"
         })
     }
@@ -75,6 +93,7 @@ fn SubProofComp<T: 'static + PartialEq + std::fmt::Display + Clone>(
     sub_proof: SubProof<T>,
     mut index: usize,
     index_map: Vec<usize>,
+    unselectable: bool,
 ) -> Element {
     let SubProof(lines) = sub_proof;
     let lines = lines.into_iter().enumerate().map(|(i, line)| {
@@ -87,7 +106,8 @@ fn SubProofComp<T: 'static + PartialEq + std::fmt::Display + Clone>(
                 let res = rsx!(SubProofComp {
                     sub_proof: s,
                     index: index,
-                    index_map: c
+                    index_map: c,
+                    unselectable
                 });
                 index += l;
                 res
@@ -100,7 +120,7 @@ fn SubProofComp<T: 'static + PartialEq + std::fmt::Display + Clone>(
                         div { class: "term-rule", "{index}:" }
                         div {
                             class: "term-line",
-                            Term { term: l, outer: true, index: c }
+                            Term { term: l, outer: true, index: c, unselectable }
                             div { class: "term-rule", "{a}" }
                         }
                     }
@@ -124,18 +144,21 @@ fn Proof() -> Element {
     let result = rsx!(Term {
         term: proof.read().result.clone(),
         outer: true,
-        index: Vec::new()
+        index: Vec::new(),
+        unselectable: true,
     });
-    let debug = use_context::<Signal<Vec<usize>>>();
+    let debug = use_context::<Signal<Option<Vec<usize>>>>();
 
     rsx! (div {
         class: "app-container",
+        "debug index: {debug:?}"
         SubProofComp {
             sub_proof: own_proof,
             index: 0,
-            index_map: Vec::new()
+            index_map: Vec::new(),
+            unselectable: false
         }
-        "Goal: {debug:?}"
+        "Goal:"
         div {
             class: "result-line",
             div { class: "term-rule", "⊢ " }
@@ -146,7 +169,7 @@ fn Proof() -> Element {
 
 fn app() -> Element {
     use_context_provider(|| Signal::new(example_proof()));
-    use_context_provider::<Signal<Vec<usize>>>(|| Signal::new(vec![]));
+    use_context_provider::<Signal<Option<Vec<usize>>>>(|| Signal::new(None));
     use_context_provider(|| Signal::new(0usize));
     let style = grass::include!("src/style.scss");
 
